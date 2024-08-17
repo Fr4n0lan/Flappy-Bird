b8875c1bfc2244cc52afd4b5ab0da4ad41da9c77
Franolan <francoeurnolan@gmail.com>
2024-08-17 15:32:00 -0400

    Rearranged files and added wing flaps

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -9,6 +9,11 @@ public class BirdScript : MonoBehaviour
     public float flapStrenght;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public GameObject wingNormal;
+    public GameObject wingFlapped;
+    private float wingTimer = 0;
+    private bool timerStarted = false;
+    public float flapTime;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +24,45 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive) {
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
+        {
             myRigidBody.velocity = Vector2.up * flapStrenght;
+            flap();
+            timerStarted = true;
         }
 
         if (transform.position.y < -17.4 || transform.position.y > 17.4)
         {
             checkBirdAndGameOver();
         }
+
+        if (timerStarted)
+        {
+            wingTimer += Time.deltaTime;
+            if (wingTimer >= flapTime)
+            {
+                timerStarted = false;
+                unflap();
+                wingTimer = 0;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         checkBirdAndGameOver();
+    }
+
+    private void flap()
+    {
+        wingFlapped.SetActive(true);
+        wingNormal.SetActive(false);
+    }
+
+    private void unflap()
+    {
+        wingFlapped.SetActive(false);
+        wingNormal.SetActive(true);
     }
 
     private void checkBirdAndGameOver()
